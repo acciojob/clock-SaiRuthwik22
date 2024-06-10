9f84a5ba28a6e5dd1ab3faffd6f2199504f8e2ca
SaiRuthwik22 <160124679+SaiRuthwik22@users.noreply.github.com>
2024-06-10 18:10:23 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 //your JS code here. If required.
+let time = document.getElementById("time")
+function dynTime(){
+	let date = new Date()
+	time.innerHTML = `${date.toLocaleString()}`
+}
+setInterval(dynTime,100)
